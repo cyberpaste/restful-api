3ca92842b9ef2fc4953b814d36ef3941ec519eac
cyberpaste <proovit08@gmail.com>
2019-02-17 18:17:52 +0300

    Пример заполнения таблиц устройства и каналы

---
+++ console/migrations/m190217_150427_add_dummy_data.php
@@ -0,0 +1,43 @@
+<?php
+
+use yii\db\Migration;
+use common\models\Devices;
+use common\models\Channels;
+use common\helpers\MeasureHelper;
+
+/**
+ * Class m190217_150427_add_dummy_data
+ */
+class m190217_150427_add_dummy_data extends Migration {
+
+    public function safeUp() {
+        $locations = ['moscow test', 'spb test'];
+
+
+        for ($i = 1; $i <= 22; $i++) {
+            $device = new Devices;
+            $device->device_id = dechex(7200000 + $i);
+            $device->type_id = rand(1, 2);
+            $device->imei = '123456789' . $i;
+            $device->install_location = $locations[array_rand($locations, 1)];
+            $device->save();
+        }
+
+        $measures = MeasureHelper::MEASURE_RELATION;
+
+        for ($i = 1; $i <= 22; $i++) {
+            $random = array_rand($measures, 1);
+            $device = new Channels;
+            $device->device_id = dechex(7200000 + $i);
+            $device->measure_unit = $random;
+            $device->measure_item = $measures[$random];
+            $device->save();
+        }
+    }
+
+    public function safeDown() {
+        $this->delete('{{%devices}}');
+        $this->delete('{{%measures}}');
+    }
+
+}
